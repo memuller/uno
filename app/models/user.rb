e30838bc946671dc260719b00e804d6c0ff9e1fb
Matheus E. Muller <hello@memuller.com>
2010-11-10 20:24:34 -0200

    Added name-handling functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,16 @@ class User
   include Mongoid::Timestamps
 
   field :email, :type => String
-  field :full_name, :type => String
+  field :names_list, :type => Array, :default => []
   field :password_hash, :type => String
 
   attr_accessor :password, :password_confirmation
+  attr_accessor :full_name
 
-  validates :email, :uniqueness => true, :format => {:with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/ }
+  validates :email, :presence => true, :uniqueness => true, :format => {:with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/ }
   validate :password_checks
-
+  
+  before_save :break_name!
   # Checks if a password matches.
   def password_checks
     if not self.password or not self.password_confirmation
@@ -22,5 +24,18 @@ class User
       self.errors.add self.password, "Password and its confirmation must match"
     end
   end
+  
+  # Name stuff
+  def name ; self.names_list[0] ; end
+  def last_name ; self.names_list[-1] ; end
+  def short_name ; "#{self.name} #{self.last_name}" ; end
+  def long_name ; self.names_list * ' ' ; end
+  def break_name!
+    if self.full_name
+      self.names_list = self.full_name.strip.split(' ')
+    end
+  end
+
+  
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,40 @@ describe User do
       User.make(:password => nil, :password_confirmation => nil).should_not be_valid
       User.make(:password => 'some', :password_confirmation => 'some').should be_valid
     end
+  end
+
+  context "basic profile info" do
+    describe "names" do
+      it "should have full_name as an array" do
+        User.new.names_list.should be_a_kind_of Array
+      end
+      it "should default to an empty array" do
+        User.new.names_list.should == []
+      end
+      it "should convert a string to an array on saving full_name" do
+        User.make!(:full_name => 'Peter Pan').names_list.should == %w(Peter Pan)
+      end
+      describe "name getters" do
+        before :all do
+          @user = User.make!(:full_name => 'First Middle Last')
+        end
+        it "long_name should return full name as string" do
+          @user.long_name.should == 'First Middle Last'
+        end
+        it "short_name should return only first and last name" do
+          @user.short_name.should == 'First Last'
+        end
+        it "name should return return the first name" do
+          @user.name.should == 'First'
+        end
+        it "last_name should return the last name" do
+          @user.last_name == 'Last'
+        end
+      end
+    end
+
+    # Pending: Sex, Location, etc
 
   end
+
 end
