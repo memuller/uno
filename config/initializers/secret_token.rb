065a543f7e25318fe8eff54f066582cae4081a45
Matheus E. Muller <hello@memuller.com>
2010-11-10 12:56:53 -0200

    Added basic rails struct

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uno::Application.config.secret_token = 'cb9309292fa944ad68c7eb45f85169794aee949e30cb3288f4d97ef304a752acde1875638d218f06ee30896214c0b886c86013470d9e6693a491ba1204b48a4a'
