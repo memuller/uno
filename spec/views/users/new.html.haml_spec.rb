69d840127f9de51fb8164b4fbb85cd96bc989ec3
Matheus E. Muller <hello@memuller.com>
2010-11-12 14:47:28 -0200

    Updated create account view.

--- spec/views/users/new.html.haml_spec.rb
+++ spec/views/users/new.html.haml_spec.rb
@@ -15,8 +15,8 @@ describe "users/new.html.haml" do
     # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
     assert_select "form", :action => users_path, :method => "post" do
       assert_select "input#user_email", :name => "user[email]"
-      assert_select "input#user_full_name", :name => "user[full_name]"
-      assert_select "input#user_password_hash", :name => "user[password_hash]"
+      assert_select "input#user_password", :name => "user[password]"
+      assert_select "input#user_password_confirmation", :name => 'user[password_confirmation]'
     end
   end
 end
