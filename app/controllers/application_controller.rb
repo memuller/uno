9aaa8178b82ac9fc17d823b09c64c3daaa5de58c
Matheus E. Muller <hello@memuller.com>
2010-11-15 17:32:12 -0200

    Added session registering methods to all controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def current_user
+    @current_user ||= login_from_session
+  end
+
+  def login_from_session
+    return nil unless session[:user_id]
+    User.find(session[:user_id])
+  end
+
+  def store_location arg = '/'
+    session[:return_to] = arg
+  end
+  
+
+  def not_authorized
+    flash[:notice] = 'Please login.'
+    store_location
+    redirect_to new_session_path
+  end
+
 end
