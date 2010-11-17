89b04ad8bf1b7d89374703ae0f1c2919589c9d66
Matheus E. Muller <hello@memuller.com>
2010-11-17 11:23:38 -0200

    Converted application template to haml.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :current_user
+
+  helper_method :current_user
 
   def current_user
     @current_user ||= login_from_session
