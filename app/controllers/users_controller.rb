97fc2695363fedad20157e476c2a37b092fd8439
Matheus E. Muller <hello@memuller.com>
2010-11-17 13:43:18 -0200

    Creating a new account now also logs you with it.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        
+        @user.session_create(self)
+
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
