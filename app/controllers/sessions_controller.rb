4ffed0d1c75e849ef56be8b36c2001a9921704c8
Matheus E. Muller <hello@memuller.com>
2010-11-17 08:29:17 -0200

    Added basic login process.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
 
   def new
-    debugger
+    if request.method.downcase == 'post'
+      if user = User.authenticate(params[:email], params[:password])
+        user.session_create(self)
+        redirect_back_or_to_root
+      elsif user == false
+        flash[:error] = "Invalid password."
+      else
+        flash[:error] = "It appears you don't have an account yet."
+      end
+    end
   end
 
   def destroy
-
+    current_user.session_destroy! self
+    redirect_back_or_to_root
   end
 end
