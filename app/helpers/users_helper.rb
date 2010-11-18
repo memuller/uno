f16908e802fb28aae93be74bc3351b79bef228b6
Matheus E. Muller <hello@memuller.com>
2010-11-18 09:11:59 -0200

    Added profile_field helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def profile_field(name, options={})
+    locals = {:field => name, :value => nil, :field_setter => name}
+    locals.merge!({:value => @current_user.send((options[:getter] || name))}) if @current_user
+    locals.merge!({:field_setter => options[:setter]}) if options[:setter]
+    render :partial => "users/fields/#{options[:type] || :text}", :locals => locals
+  end
+
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,4 +11,28 @@ require 'spec_helper'
 #   end
 # end
 describe UsersHelper do
+  describe "the profile_field constructor" do
+    it "should have the profile_field method" do
+      helper.should respond_to :profile_field
+    end
+    it "should receive field_name as parameter" do
+      lambda{helper.profile_field()}.should raise_error ArgumentError
+      lambda{helper.profile_field(:name)}.should_not raise_error ArgumentError
+    end
+    it "should receive any other parameters" do
+      lambda{helper.profile_field(:name, :thing => 'true')}.should_not raise_error ArgumentError
+    end
+
+    context "when building text fields" do
+      describe "the result" do
+        it "should have a label"
+        it "its field id should be in the user_name format"
+        it "its name should be in the user[name] format"
+
+      end
+    end
+
+  end
+  describe "profile form builder" do
+  end
 end
