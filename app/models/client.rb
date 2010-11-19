b68de41288be0481e41b999ea71fa700a9a2c314
Matheus E. Muller <hello@memuller.com>
2010-11-19 13:36:12 -0200

    Added basic client persistence behavior.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,6 @@
 class Client
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name, :type => String
   field :url, :type => String
   field :api_key, :type => String
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Client do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "basic attributes" do
+    it "should be a MongoID object" do
+      Client.ancestors.should include Mongoid::Document
+    end
+    it "should have an url" do 
+      Client.make.should respond_to :url
+    end
+    it "should have name" do 
+      Client.make.should respond_to :name
+    end
+    it "should have timestamps" do 
+      Client.make.should respond_to :created_at
+      Client.make.should respond_to :updated_at
+    end
+  end
+
+  describe "user relationships" do
+  end
 end
