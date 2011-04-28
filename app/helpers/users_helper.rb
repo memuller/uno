0444d521ed54cd47f1e119eeca50153095cbe8df
Matheus E. Muller <hello@memuller.com>
2011-04-28 09:04:45 -0300

    Refactors profile_field builders.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,10 @@
 module UsersHelper
   def profile_field(name, options={})
     locals = {:field => name, :value => nil, :field_setter => name}
-    locals.merge!({:value => @current_user.send((options[:getter] || name))}) if @current_user
-    locals.merge!({:field_setter => options[:setter]}) if options[:setter]
+    options.merge! User.profile_fields[name]
+    locals.merge!({:value => @current_user.send((options[:getter] || name))}) and options.delete(:getter) if @current_user
+    locals.merge!({:field_setter => options[:setter]}) and options.delete(:setter) if options[:setter]
+    locals.merge! options
     render :partial => "users/fields/#{options[:type] || :text}", :locals => locals
   end
 
